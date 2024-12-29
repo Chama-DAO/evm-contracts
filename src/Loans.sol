718496e36b937f466bae7c839ac14421dc4f645c
ybtuti <samuelmsuu@gmail.com>
2024-12-29 23:16:17 +0300

    Add Makefile and initial loan-related contracts with tests

---
+++ src/Loans.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Loans is ReentrancyGuard {
+// This contract handles loans logic for the chamas
+}
